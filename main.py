a45d696316374a986d7dd862934e77fb260fb66d
acloe <acloe1@gmail.com>
2025-05-07 22:30:34 -0700

    Add player spaceship drawing functionality

--- main.py
+++ main.py
@@ -4,17 +4,28 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock=pygame.time.Clock()
+    dt=0
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        player.draw(screen)
+        #dt delta time in seconds
+        dt = clock.tick(60)/1000
+       
+        
         
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
