d98de4fb5c1e8d6646ea0b4c62db9f0447eef8d7
acloe <acloe1@gmail.com>
2025-05-12 15:33:54 -0700

    using groups to manage objects

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def main():
     clock=pygame.time.Clock()
     dt=0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
@@ -21,11 +26,12 @@ def main():
         #calculate dt
         dt = clock.tick(60)/1000
         
-        player.update(dt)
+        updatable.update(dt)
         
         #draw everything
         screen.fill((0,0,0))
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         
         #display the frame
         pygame.display.flip()
